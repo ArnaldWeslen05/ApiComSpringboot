eefc9dfabebd28c84e91db47c749775c77f8b228
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-07-30 23:14:12 -0300

    hash de senha e tabela task

--- todolist/src/main/java/br/com/arnaldweslen/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/arnaldweslen/todolist/user/UserController.java
@@ -1,8 +1,12 @@
 package br.com.arnaldweslen.todolist.user;
 
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +27,9 @@ public class UserController {
         if( user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
         }
+       var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+       
+       userModel.setPassword(passwordHashred);
       
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
